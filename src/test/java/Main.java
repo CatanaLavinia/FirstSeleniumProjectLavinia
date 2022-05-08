59c01e759a338f5dabfe5b50c11506826c45ba9d
Lavinia Catana <lavinia.catana@yahoo.com>
2022-05-08 13:06:56 +0300

    Lab 16 - Register&Wish List Tests

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,21 @@
+import javax.imageio.spi.RegisterableService;
 
 public class Main {
 
     public static void main(String[] args) {
-        LoginTest login = new LoginTest();
-        login.loginWithValidCredentialsTest();
-        login.loginWithInvalidEmail();
-        login.loginWithInvalidPasswordTest();
-        login.loginWithoutCredentialsTest();
+//        LoginTest login = new LoginTest();
+//        login.loginWithValidCredentialsTest();
+//        login.loginWithInvalidEmail();
+//        login.loginWithInvalidPasswordTest();
+//        login.loginWithoutCredentialsTest();
+
+//        RegisterTest register = new RegisterTest();
+//        register.validRegisterTest();
+
+        WishListTest wishList = new WishListTest();
+        wishList.addProductToWishList();
+
+
     }
 
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class RegisterTest {
 
@@ -8,6 +10,14 @@ public class RegisterTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
-
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Catana");
+        driver.findElement(By.id("lastname")).sendKeys("Lavinia");
+        driver.findElement(By.id("email_address")).sendKeys("lavinia.catana.11@yahoo.com");
+        driver.findElement(By.id("password")).sendKeys("Test.123");
+        driver.findElement(By.id("confirmation")).sendKeys("Test.123");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.close();
     }
 }
